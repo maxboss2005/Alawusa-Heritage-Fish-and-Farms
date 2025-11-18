a9e8a1985b84eb8a2b0a3b9c558051ea3100b596
Matthew Idowu <babatundemoyo05@gmail.com>
2025-11-17 23:59:28 -0800

    Update usercart.js

--- usercart.js
+++ usercart.js
@@ -241,7 +241,7 @@ async function checkout() {
       localStorage.setItem('paymentRedirect', JSON.stringify(redirectData));
 
       FlutterwaveCheckout({
-        public_key: "FLWPUBK-12f39e50a0c4450e5c4cfb2a3151a57a-X",
+        public_key: "FLWPUBK_TEST-ddaa66dfb199659668d82c30f198226a-X",
         tx_ref: txRef,
         amount: totalAmount,
         currency: "NGN",
@@ -721,3 +721,4 @@ updateCartFunctions();
 
 
     
+
